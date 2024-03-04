0cfbf20e3e092fddd670e176210ce500c773bb2c
gudin10 <carlosandresgudin@gmail.com>
2024-03-03 21:02:06 -0500

    pedir-carta

--- assets/js/juego.js
+++ assets/js/juego.js
@@ -5,10 +5,12 @@
  * 2S = Tow of Spades
  */
 
+
 let deck = [];
 const tipos = ['C','D','H','S'];
 const especiales = ['A','J','Q','K'];
 
+//Esta funcion crea un nuevo decko baraja
 const crearDeck = ()=>{
     for( let i =2; i<=10 ;i++){
         for( let tipo of tipos){
@@ -22,10 +24,48 @@ const crearDeck = ()=>{
             deck.push( especial + tipo);
         }
     }
-    console.log( deck );
+    //console.log( deck );
 
     deck = _.shuffle( deck);
     console.log( deck );
+
+    return deck;
 }
 
 crearDeck();
+
+const pedirCarta = ()=>{
+
+    if( deck.length ===0){
+        throw 'No hay cartas en el deck';
+    }
+    const carta = deck.pop(); 
+
+    console.log( carta );
+    return carta;
+}
+
+//pedirCarta();
+
+const valorCarta = (carta)=>{
+    //let puntos = 0;
+    // if( isNaN( valor) ){
+    //     puntos = (valor === 'A') ? 11 : 10;
+
+    //     console.log('No es un numero');
+    // }else{
+    //     console.log('Es un numero');
+    //     puntos = valor * 1;//tranformarlo en entero
+    // }
+
+    // console.log(puntos);
+    const valor = carta.substring(0,carta.length -1);
+
+    return (isNaN(valor)) ?
+            (valor==='A') ? 11 :10
+            : valor *1
+
+}
+
+const valor = valorCarta(pedirCarta());
+console.log(valor);
